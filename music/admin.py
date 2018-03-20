08af2b70ce75a7dd44e3ce64ba7d29d043fa93c4
esskay <sidforyou14@gmail.com>
2018-03-20 19:49:33 +0530

    now admin can see users most played list

--- music/admin.py
+++ music/admin.py
@@ -9,9 +9,24 @@ class MyUserAdmin(UserAdmin):
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         if extra_context is None:
-            # Add Here
-        return super(MyUserAdmin, self).change_view(
-            request, object_id=object_id, form_url=form_url, extra_context=extra_context)
+
+            etc="select id,song_title,SUM(played_counter) AS sum from music_song where album_id in (select id from music_album where user_id = {0}) group by song_title  having sum >4 LIMIT 10".format(object_id)
+            temp = Song.objects.raw(etc)
+
+
+            songname=[]
+            songcount=[]
+
+
+            for song in temp:
+                 songname.append(song.song_title)
+                 songcount.append(int(song.sum))
+
+            extra_context = {"try": etc,"songname": songname,"songcount": songcount}
+
+
+            return super(MyUserAdmin, self).change_view(
+                request, object_id=object_id, form_url=form_url, extra_context=extra_context)
 
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
